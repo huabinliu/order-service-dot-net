16ece4b9b000706b5dfca716497732138c144f3a
linwenjun <linwenjun@163.com>
2019-07-26 23:05:23 +0800

    change Dockerfile

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+LOCAL_TAG=cloud-native-$TEAM:$SERVICE-$BUILD_NUMBER
+REMOTE_TAG=$ECR_HOST/$LOCAL_TAG
+
 username=$(cat /etc/docker-registry/username)
 password=$(cat /etc/docker-registry/password)
 endpoint=$(cat /etc/docker-registry/endpoint)
@@ -10,6 +13,6 @@ sudo docker login -u $username -p $password $endpoint
 
 TAG=$SERVICE-$BRANCH_NAME-$BUILD_NUMBER
 
-sudo docker build -t $TEAM:$TAG .
-sudo docker tag $TEAM:$TAG $ECR_HOST/$TEAM:$TAG
-sudo docker push $ECR_HOST/cloud-native-$TEAM:$TAG
+sudo docker build -t $LOCAL_TAG -f Dockerfile.aws .
+sudo docker tag $LOCAL_TAG $REMOTE_TAG
+sudo docker push $REMOTE_TAG
