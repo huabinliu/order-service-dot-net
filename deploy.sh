d052070708b747c662c1d8297b168d10be540794
linwenjun <linwenjun@163.com>
2019-07-26 23:18:15 +0800

    fix directory issue

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ REMOTE_TAG=$ECR_HOST/$LOCAL_TAG
 PROFILES=$1
 NAMESPACE="$TEAM-$PROFILES"
 
-sed "s#{{profiles}}#$PROFILES#g; s#{{team}}#$TEAM#g; s#{{image}}#$REMOTE_TAG#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" ci/kube.yaml
+sed "s#{{profiles}}#$PROFILES#g; s#{{team}}#$TEAM#g; s#{{image}}#$REMOTE_TAG#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" kube.yaml
 
-sed "s#{{profiles}}#$PROFILES#g; s#{{team}}#$TEAM#g; s#{{image}}#$REMOTE_TAG#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" ci/kube.yaml | sudo kubectl --kubeconfig $KUBE_CONFIG apply -f -
+sed "s#{{profiles}}#$PROFILES#g; s#{{team}}#$TEAM#g; s#{{image}}#$REMOTE_TAG#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" kube.yaml | sudo kubectl --kubeconfig $KUBE_CONFIG apply -f -
