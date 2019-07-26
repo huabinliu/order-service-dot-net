d19e14af48e415ae144426c2a14e08c8681da043
linwenjun <linwenjun@163.com>
2019-07-26 23:19:45 +0800

    fix ns issue

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 LOCAL_TAG=twuc-$TEAM:$SERVICE-$BUILD_NUMBER
 REMOTE_TAG=$ECR_HOST/$LOCAL_TAG
-PROFILES=$1
+PROFILES=dev
 NAMESPACE="$TEAM-$PROFILES"
 
 sed "s#{{profiles}}#$PROFILES#g; s#{{team}}#$TEAM#g; s#{{image}}#$REMOTE_TAG#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" kube.yaml
