af46388c340b4b2b2a5076e02372eec728f42265
linwenjun <linwenjun@163.com>
2019-07-26 22:52:33 +0800

    add dockerfile.aws

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -ex
 
 TAG=$SERVICE-$BRANCH_NAME-$BUILD_NUMBER
 
-sudo docker build -t $TEAM:$TAG .
+sudo docker build -t $TEAM:$TAG -f Dockerfile.aws
 sudo docker tag $TEAM:$TAG $ECR_HOST/$TEAM:$TAG
 sudo docker push $ECR_HOST/$TEAM:$TAG
