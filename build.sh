717c7f4e54b77fe74a2fa370534d4ef8e2c3b090
linwenjun <linwenjun@163.com>
2019-07-26 22:58:27 +0800

    change dockerfile

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 
 set -ex
 
+username=$(cat /etc/docker-registry/username)
+password=$(cat /etc/docker-registry/password)
+endpoint=$(cat /etc/docker-registry/endpoint)
+
+sudo docker login -u $username -p $password $endpoint
+
 TAG=$SERVICE-$BRANCH_NAME-$BUILD_NUMBER
 
-sudo docker build -t $TEAM:$TAG -f Dockerfile.aws
+sudo docker build -t $TEAM:$TAG .
 sudo docker tag $TEAM:$TAG $ECR_HOST/$TEAM:$TAG
 sudo docker push $ECR_HOST/$TEAM:$TAG
