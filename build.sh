7158abf163190ed6a550c63737cb59438b6d864f
linwenjun <linwenjun@163.com>
2019-07-26 23:01:24 +0800

    chang build.sh

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ TAG=$SERVICE-$BRANCH_NAME-$BUILD_NUMBER
 
 sudo docker build -t $TEAM:$TAG .
 sudo docker tag $TEAM:$TAG $ECR_HOST/$TEAM:$TAG
-sudo docker push $ECR_HOST/$TEAM:$TAG
+sudo docker push $ECR_HOST/cloud-native-$TEAM:$TAG
